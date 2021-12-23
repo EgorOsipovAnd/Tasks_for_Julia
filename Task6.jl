e239fe5ab7002b9e0c0bce7100c766111833a3e2
Егор <90093321+EgorOsipovAnd@users.noreply.github.com>
2021-12-24 00:26:58 +0300

    Add files via upload

--- Task6.jl
+++ Task6.jl
@@ -1,105 +1,122 @@
 #Ost - "E6" - восток, Nord - "N8" - север, West - "W4" - запад, Sud - "S2" - юг
-"""
-main_function6!(r) - главная функция
-"""
-function main_function6!(r)
-    side = Ost
-    arr = []
-    exit_to_the_lower_left_cell!(r, arr)
-    up_search(r, side)
+function size(r)
+    countx = 0
+    county = 0
+    countx = move_to_wall(r,Ost)
+    move_to_wall(r,West)
+    county = move_to_wall(r, Nord)
+    move_to_wall(r,Sud)
+    return countx, county
 end
-"""
-exit_to_the_lower_left_cell!(r, arr) - это функция заполняет в переданый массив  двумя сторонами West || Sud: принцип алгоритма такой
-цикл while работет так: пока слева(West) && снизу(Sud) нет перегоротки каждая проверка будет выполнятся проверка двух if: первый проверяет 
-есть ли перегоротка в заданном направлении, если нет, то робот делает шаг в сторону West и заносит это сторону в переданный массив 
-принцип работы второго if такой же.Цикл while прекратит свою работу, когда окажется в левом нижнем углу
-"""
-function exit_to_the_lower_left_cell!(r, arr)
-    while !(isborder(r, West)&&isborder(r, Sud))
-        if !isborder(r, West)
-            move!(r, West)
-            push!(arr, West)
-        end
-        if !isborder(r, Sud)
-            move!(r, Sud)
-            push!(arr, Sud)
-        end
+
+function next_side(side)
+    if side == Nord
+        return Ost
+    end
+    if side == Sud
+        return West
+    end
+    if side == West
+        return Nord
+    end
+    if side == Ost
+        return Sud
     end
 end
-"""
-return_to_starting_point!(r, arr) - это функция использует переданный массив и передвигается по элементам массива то есть пока длина массива
-не будет равна нулю выполняется следующие переменная side принимает значение последнего элемента массива и двигается в противоположном направление за это отвечает фунrция inverse
-дальше удаляется последний элемент массива и так пока длина массива не станет равна 0
-"""
-function return_to_starting_point!(r, arr)
-    while length(arr)>0
-        side=pop!(arr)
-        move!(r, inverse(side))
+
+function move_to_wall(r, side)
+    count = 0
+    while isborder(r,side)==false
+        move!(r,side)
+        count += 1
+    end
+    return count
+end
+
+function move_to_wall_nc(r, side, ncount)
+    count = 0
+    while isborder(r,side)==false
+        move!(r,side)
+        count += 1
+    end
+    if count < ncount
+        putmarker!(r)
+        return 1
+    else
+        move_to_wall(r,Sud)
+        return 0
     end
 end
-"""
-inverse(side::HorizonSide) - получает переданую в функцию side преобразовывает её в Int значение то есть Nord = 0,
-West = 1, Sud = 2, Ost = 3 потом преобразовывает в сторону противоположную : допустим, что в функцию передано напрвление West после преобразование в значения Int
-West = 1 после к этого к этому числу прибавляется 2 и берется остаток от деления на число 4 то есть 3 и это сторона Ost, а эта сторона противоположна стороне West
-"""
-function inverse(side)
-    HorizonSide(mod(Int(side)+2, 4))
-end
-"""
-changing_side_to_clock_side(side) - получает переданую в функцию side преобразовывает её в Int значение то есть Nord = 0,
-West = 1, Sud = 2, Ost = 3 потом преобразовывает её на часовую сторону : допустим, что в функцию передано напрвление West после преобразование в значения Int
-Sud = 2 после к этого к этому числу прибавляется 1 и берется остаток от деления на число 4 то есть 3 и это сторона Ost, а эта часовая сторона Sud
-"""
-function changing_side_to_clock_side1(side)
-    HorizonSide(mod(Int(side)+1, 4))
-end
-"""
-changing_side_to_clock_side(side) - получает переданую в функцию side преобразовывает её в Int значение то есть Nord = 0,
-West = 1, Sud = 2, Ost = 3 потом преобразовывает её на часовую сторону : допустим, что в функцию передано напрвление West после преобразование в значения Int
-Sud = 2 после к этого к этому числу прибавляется 3 и берется остаток от деления на число 4 то есть 1 и это сторона Sud, а эта часовая сторона West
-"""
-function changing_side_to_clock_side(side)
-    HorizonSide(mod(Int(side)+3, 4))
-end
-"""
-inner_perimeter(r, side) - функция ставит маркеры по наружному периметру пока в клетке с роботом не будет маркера алгоритм функции такой:
-пока есть перегоротка в заданном направлении исполняется список команд поставить маркер в клетке с роботом и сделать шаг в заданном переменной направление 
-после цикла while происходит доп проверка есть ли маркер в клетке с роботом если нет то выполняется список команд поставить маркер, шаг от измененной переменной
-и присвоение переменной нового значения стороны
-"""
-function inner_perimeter(r, side)
-    side1 = side
-    while !ismarker(r)
-        while isborder(r, side1) == true
-            putmarker!(r)
-            if !ismarker(r)
-                move!(r, changing_side_to_clock_side1(side1))
-            end
-        end
-        if !ismarker(r)
-            putmarker!(r)
-            move!(r, side1)
-            side1 = changing_side_to_clock_side(side)
-        end
+
+function rev(side)
+    if side == Nord
+        return Sud
+    end
+    if side == Sud
+        return Nord
+    end
+    if side == West
+        return Ost
+    end
+    if side == Ost
+        return West
     end
 end
-"""
-up_search(r, side) - это функция поднимается вверх и ищет сверху перегоротку
-"""
-function up_search(r, side)
-    try_ch = true
-    while try_ch == true
-        while !isborder(r, side)
-            move!(r, side)
-            if isborder(r, Nord)
-                inner_perimeter(r, Nord)
-                try_ch = false
-            end
+
+function corner(r, side1, side2)
+    path = []
+    while (isborder(r,side1)&&isborder(r,side2))==false
+        if isborder(r,side1)==false
+            move!(r,side1)
+            push!(path,rev(side1))
         end
-        side = inverse(side)
-        if isborder(r, Nord)
-            try_ch = false
+        if isborder(r,side2)==false
+            move!(r,side2)
+            push!(path,rev(side2))
         end
-        move!(r, Nord)
     end
+    return path    
+end
+
+function way_back(r,path)
+    for i in 0:(length(path)-1)
+        move!(r,path[length(path)-i])
+    end
+end
+
+function mark_arround(r)
+    path = corner(r,Sud,West)
+    sizex,sizey = size(r)
+    while 1 == 1
+        f = move_to_wall_nc(r,Nord,sizey)
+        if f == 1
+            break
+        end
+        move!(r,Ost)
+    end
+    while isborder(r,Nord) == true
+        putmarker!(r)
+        move!(r,Ost)
+        putmarker!(r)
+    end
+    move!(r,Nord)
+    while isborder(r,West) == true
+        putmarker!(r)
+        move!(r,Nord)
+        putmarker!(r)
+    end
+    move!(r,West)
+    while isborder(r,Sud) == true
+        putmarker!(r)
+        move!(r,West)
+        putmarker!(r)
+    end
+    move!(r,Sud)
+    while isborder(r,Ost) == true
+        putmarker!(r)
+        move!(r,Sud)
+        putmarker!(r)
+    end
+    corner(r,West,Sud)
+    way_back(r,path)
 end
